ad01788d04bfa14ff94d9650d54823776bfdfbf7
zk <ke.zhang@zthd.io>
2025-05-06 18:19:04 +0800

    kingfisher WebP支持和缓存设置

--- SwiftUIPreview/SwiftUIPreviewApp.swift
+++ SwiftUIPreview/SwiftUIPreviewApp.swift
@@ -6,9 +6,28 @@
 //
 
 import SwiftUI
+import Kingfisher
+import KingfisherWebP
 
 @main
 struct SwiftUIPreviewApp: App {
+    
+    init() {
+        // WebP支持
+        KingfisherManager.shared.defaultOptions += [
+          .processor(WebPProcessor.default),
+          .cacheSerializer(WebPSerializer.default),
+//          .cacheOriginalImage
+        ]
+        
+        // 限制图片缓存大小为当前设备最大物理内存的5%
+        let defaultImageCache = KingfisherManager.shared.cache
+        let totalRAM = ProcessInfo.processInfo.physicalMemory
+        defaultImageCache.memoryStorage.config.totalCostLimit = Int(totalRAM / 20)
+        defaultImageCache.diskStorage.config.sizeLimit = 500 * 1024 * 1024  // 500MB 磁盘缓存
+        
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
